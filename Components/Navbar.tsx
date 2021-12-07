6194ab4272d12957c4006941705f3736b58020b0
Francisco <fveranicola@gmail.com>
2021-12-06 20:28:59 -0500

    Creating cards with material ui for restaurant results and adding google maps api

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import Link from 'next/link';
 import styles from '../styles/Navbar/navbar.module.css';
+import Image from 'next/Image';
+
+const myLoader = ({ src, width, quality }: any) => {
+  return `https://i.imgur.com/${src}?w=${width}&q=${quality || 75}`
+}
+
 
 const Navbar = () => {
   return (
     <div className={styles.navOutterContainer }>
       <nav className={styles.navbar}>
         <div className={styles.navbarLogoContainer}>
-          Munch Hunt
+          <Image
+          loader={myLoader}
+          src="/e1L61T3.png"
+          alt="Munch Hunt logo"
+          width={60}
+          height={60}
+          />
+          <h2>Munch Hunt</h2>
         </div>
         <div className={styles.navbarLinkContainer}>
           <Link href="/find"><a className={styles.navLinks}>Find</a></Link>
--- pages/results.tsx
+++ pages/results.tsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import type { NextPage } from 'next'
 import styles from '../styles/Results/results.module.css';
+import FoodResults from '../Components/Results/FoodResults';
+import Head from 'next/head';
 
 const Results: NextPage = () => {
   return (
     <div>
-      <h1>Results</h1>
+      {/* <Head>
+        <script async
+         src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
+        </script>
+      </Head> */}
+      <div className={styles.foodChoiceDiv}>
+        <div className={styles.foodChoiceBox}>
+          <div className={styles.foodChoiceTitle}>
+            <h4>Munch Hunt chose:</h4>
+          </div>
+          <div className={styles.foodChoiceResult}>
+            American (Fast Food)
+          </div>
+        </div>
+      </div>
       <div className={styles.outterBox}>
         <div className={styles.innerBox}>
           <div className={styles.resultsColumns}>
-            result columns
-            import component that renders results
+            <FoodResults />
           </div>
           <div className={styles.mapBox}>
             map/ detail boxes
