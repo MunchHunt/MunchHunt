5aa80f05cf2fd907d900f02c4f17aa2d7d50d19a
Seiji Matsumoto <seijim27@gmail.com>
2021-12-06 19:41:09 -0500

    Added loading indicator for long lat

--- Components/Home/Location.tsx
+++ Components/Home/Location.tsx
@@ -1,15 +1,35 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   currLocation: string;
   setCurrLocation: Function;
 }
+
 const Location: React.FC<Props> = ({ currLocation, setCurrLocation }) => {
   const [locationInput, setLocationInput] = useState<string>("");
+  const [long, setLong] = useState<string>('');
+  const [lat, setLat] = useState<string>('');
+  const [showLoad, setShowLoad] = useState<boolean>(false);
 
   const getCurrentPosition = () => {
-
+    if (long.length === 0) {
+      setShowLoad(true);
+    }
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setLong(position.coords.longitude.toString());
+        setLat(position.coords.latitude.toString());
+      });
+    } else {
+      alert("Sorry, your browser does not support HTML5 geolocation.");
+    }
   };
+
+  useEffect(() => {
+    if (long.length) {
+      setShowLoad(false);
+    }
+  }, [long]);
 
   return (
     <div>
@@ -24,6 +44,10 @@ const Location: React.FC<Props> = ({ currLocation, setCurrLocation }) => {
           }}
         />
       </label>
+      <button onClick={getCurrentPosition}>Current Location</button>
+      {showLoad ? <h3>Loading...</h3> : null}
+      <div>{long}</div>
+      <div>{lat}</div>
     </div>
   );
 };
