4ae54e06890cb2d06e6c7e1ce95cc23d2b645c50
Francisco <fveranicola@gmail.com>
2021-12-30 19:29:29 -0500

    othe minor styling changes

--- Components/Results/results/Tab.tsx
+++ Components/Results/results/Tab.tsx
@@ -20,7 +20,7 @@ function TabPanel(props: any) {
     >
       {value === index && (
         <Box sx={{ p: 0 }}>
-          <div>{ children }</div>
+          <div>{children}</div>
         </Box>
       )}
     </div>
@@ -32,7 +32,7 @@ TabPanel.propTypes = {
   index: PropTypes.number.isRequired,
   value: PropTypes.number.isRequired,
 };
-function a11yProps(index:any) {
+function a11yProps(index: any) {
   return {
     id: `simple-tab-${index}`,
     'aria-controls': `simple-tabpanel-${index}`,
@@ -43,23 +43,23 @@ export default function BasicTabs(props: any) {
   const maps = props.maps;
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event: any, newValue:any) => {
+  const handleChange = (event: any, newValue: any) => {
     setValue(newValue);
   };
 
   return (
-    <Box  className={styles.tabContainer} sx={{ width: '100%'}}>
+    <Box className={styles.tabContainer} sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
         <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-          <Tab label="Map" {...a11yProps(0)} />
-          <Tab label="Details" {...a11yProps(1)} />
+          <Tab label="Details" {...a11yProps(0)} />
+          <Tab label="Map" {...a11yProps(1)} />
         </Tabs>
       </Box>
       <TabPanel value={value} index={0}>
-      <GoogleMaps defaultZoom={maps.defaultZoom} localRestaurants={maps.localRestaurants} defaultCenter={{ lat: maps.defaultCenter.lat, lng: maps.defaultCenter.lng }} />
+        <Details details={props.details} />
       </TabPanel>
       <TabPanel value={value} index={1}>
-        <Details details={props.details}/>
+        <GoogleMaps defaultZoom={maps.defaultZoom} localRestaurants={maps.localRestaurants} defaultCenter={{ lat: maps.defaultCenter.lat, lng: maps.defaultCenter.lng }} />
       </TabPanel>
     </Box>
   );
